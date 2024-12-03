40cf07fc123fde27e6d87177a5f42375a6fd07eb
msvtlkh <mariia@Noutbuk-Mariia.lan>
2024-12-03 20:37:10 +0800

    add comment

--- vite-project/src/main.tsx
+++ vite-project/src/main.tsx
@@ -2,7 +2,7 @@ import { createRoot } from 'react-dom/client'
 import App from './components/App'
 import { Provider } from 'react-redux'
 import { store } from './state/store'
-
+//
 createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
     <App />
